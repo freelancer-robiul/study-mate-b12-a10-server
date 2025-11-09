5222710ecb08c381bd8bb2617ccfe7372ae00a2e
Robiul Islam <robi.kushtia@gmail.com>
2025-11-10 01:53:49 +0600

    MongoDB connected

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const uri =
+  "mongodb+srv://B12-A10-server:cPphu7RoRa9DP6Si@b12-a10-server.g9l3hzm.mongodb.net/?appName=B12-A10-server";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("✅ Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error);
+  }
+}
+
+run().catch(console.dir);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server app listening on port ${port}`);
 });
